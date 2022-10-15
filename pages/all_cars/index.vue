8b9fea5600e3549487a814077dce9abf4ba58b70
Ize Phanthakarn <izephanthakarn@hotmail.com>
2022-10-16 03:21:36 +0700

    redesign all cars page

--- pages/all_cars/index.vue
+++ pages/all_cars/index.vue
@@ -4,8 +4,140 @@
             <base-icon icon="car-sideview" size="40"></base-icon>
             <h1 class="text-3xl font-medium">All Cars</h1>
         </div>
-        <div class="w-full overflow-y-auto grid grid-cols-4 gap-5 mt-5 rounded-xl">
-            <!-- Category -->
+        <div class="w-full h-full mt-5 grid grid-cols-4 gap-5">
+            <div class="p-5 bg-gray3 rounded-xl">
+                <div class="bg-gray3 h-full rounded-xl">
+                    <div class="w-full flex items-center space-x-3">
+                        <base-icon icon="category" size="36"></base-icon>
+                        <h1 class="text-3xl font-semibold">Category</h1>
+                    </div>
+                    <div class="w-full mt-3 overflow-y-auto" id="menu">
+                        <ul class="category-list font-medium text-lg">
+                            <li class="py-1.5 px-3 mb-1 cursor-pointer hover:text-white rounded-lg flex items-center space-x-2"
+                                :class="{'bg-gray4 text-white':header=='All Cars'}" @click="title('All Cars')">
+                                <div class="w-7 h-7 bg-gray rounded-full flex items-center justify-center">
+                                    <base-icon icon="car-sideview" size="24" class="text-white"></base-icon>
+                                </div>
+                                <p>All Cars</p>
+                            </li>
+                            <li class="py-1.5 px-3 my-1 cursor-pointer rounded-lg flex items-center space-x-2"
+                                :class="{'bg-gray4':header=='BMW'}" @click="title('BMW')">
+                                <div class="w-7 h-7 bg-gray rounded-full flex items-center justify-center">
+                                    <img src="img/logo/BMW.png" alt="" class="w-6">
+                                </div>
+                                <p :class="{'text-white':header=='BMW'}">BMW</p>
+                            </li>
+                            <li class="py-1.5 px-3 my-1 cursor-pointer rounded-lg flex items-center space-x-2"
+                                :class="{'bg-gray4 text-white':header=='Audi'}" @click="title('Audi')">
+                                <div class="w-7 h-7 bg-gray rounded-full flex items-center justify-center">
+                                    <img src="img/logo/Audi.png" alt="" class="w-5">
+                                </div>
+                                <p>Audi</p>
+                            </li>
+                            <li class="py-1.5 px-3 my-1 cursor-pointer rounded-lg flex items-center space-x-2"
+                                :class="{'bg-gray4 text-white':header=='Mercedes-Benz'}"
+                                @click="title('Mercedes-Benz')">
+                                <div class="w-7 h-7 bg-gray rounded-full flex items-center justify-center">
+                                    <img src="img/logo/Benz.png" alt="" class="w-5">
+                                </div>
+                                <p>Mercedes-Benz</p>
+                            </li>
+                            <li class="py-1.5 px-3 my-1 cursor-pointer rounded-lg flex items-center space-x-2"
+                                :class="{'bg-gray4 text-white':header=='Honda'}" @click="title('Honda')">
+                                <div class="w-7 h-7 bg-gray rounded-full flex items-center justify-center">
+                                    <img src="img/logo/Honda.png" alt="" class="w-6">
+                                </div>
+                                <p>Honda</p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-span-3 h-full bg-gray3 p-5 rounded-xl flex flex-col">
+                <div class="w-full h-10 flex">
+                    <div v-show="header == 'All Cars'"
+                        class="w-10 h-10 bg-gray rounded-full flex items-center justify-center">
+                        <base-icon class="text-white" icon="car-sideview" size="36"></base-icon>
+                    </div>
+                    <div v-show="header == 'BMW'"
+                        class="w-10 h-10 bg-gray rounded-full flex items-center justify-center">
+                        <img src="img/logo/BMW.png" alt="" class="w-9">
+                    </div>
+                    <div v-show="header == 'Audi'"
+                        class="w-10 h-10 bg-gray rounded-full flex items-center justify-center">
+                        <img src="img/logo/Audi.png" alt="" class="w-8">
+                    </div>
+                    <div v-show="header == 'Mercedes-Benz'"
+                        class="w-10 h-10 bg-gray rounded-full flex items-center justify-center">
+                        <img src="img/logo/Benz.png" alt="" class="w-7">
+                    </div>
+                    <div v-show="header == 'Honda'"
+                        class="w-10 h-10 bg-gray rounded-full flex items-center justify-center">
+                        <img src="img/logo/Honda.png" alt="" class="w-9">
+                    </div>
+
+                    <h1 class="text-3xl font-semibold ml-3">{{header}}</h1>
+                </div>
+                <hr class="border-gray-400 mb-4 mt-3">
+                <div class="w-full flex justify-between">
+                    <base-input label="Car's name" class="w-96" />
+                    <div class="flex items-center space-x-3">
+                        <div>
+                            <h1>Sort By</h1>
+                            <div class="select w-44 text-black pl-2.5">
+                                <select name="" id="">
+                                    <option value="all">All</option>
+                                    <option value="lasted">Lasted to Oldest</option>
+                                    <option value="oldest">Oldest to Lasted</option>
+                                    <option value="az">A to Z</option>
+                                    <option value="za">Z to A</option>
+                                    <option value="popular">Popular</option>
+                                    <option value="rating">Rating</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                <h1>Item Per Page</h1>
+                                <div class="select text-black w-20 px-3">
+                                    <select name="" id="">
+                                        <option value=6 selected>6</option>
+                                        <option value=12>12</option>
+                                        <option value=18>18</option>
+                                        <option value=24>24</option>
+                                        <option value=30>30</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="w-full mt-5 grid grid-cols-3 gap-5 text-white rounded-lg overflow-y-auto" id="content">
+                    <div class="card bg-gray5 rounded-lg p-4" v-for="i in 5" :key="i">
+                        <img src="img/cars/BMW-M8.jpg" alt="" class="rounded-xl">
+                        <h1 class="mt-2 text-xl font-medium">BMW M8</h1>
+                        <h2>Brand: BMW</h2>
+                        <h3>Type: Sport Car</h3>
+                        <h4>Price: {{price("17959000")}} Baht</h4>
+                        <h5>Seller: <span class="highlight">Phanthakarn Khumphai</span></h5>
+                        <h6 v-if="showroom != null">Showroom: <span class="highlight">Showroom's name</span></h6>
+                        <h6 v-if="showroom == null">Province: Bangkok</h6>
+                        <div class="w-full mt-3 flex justify-between">
+                            <div class="flex items-center space-x-1">
+                                <base-icon icon="visibility" size="24" class="text-primary"></base-icon>
+                                <p>10.7 K</p>
+                            </div>
+                            <button class="duration-200 hover:scale-110">
+                                <base-icon icon="heart-regular" size="28" class="text-primary"></base-icon>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- <div class="w-full overflow-y-auto grid grid-cols-4 gap-5 mt-5 rounded-xl">
+
             <div class="bg-gray3 p-5 rounded-xl">
                 <div class="w-full flex items-center space-x-3">
                     <base-icon icon="category" size="36"></base-icon>
@@ -49,7 +181,7 @@
                     </li>
                 </ul>
             </div>
-            <!-- Content -->
+            Content
             <div class="col-span-3 bg-gray3 p-5 rounded-xl">
                 <div class="w-full h-10 flex">
                     <div v-show="header == 'All Cars'"
@@ -107,7 +239,7 @@
                     </div>
                 </div>
                 <div class="w-full mt-5 grid grid-cols-3 gap-5 text-white">
-                    <div class="card bg-gray5 rounded-lg p-4" v-for="i in 30" :key="i">
+                    <div class="card bg-gray5 rounded-lg p-4" v-for="i in 2" :key="i">
                         <img src="img/cars/BMW-M8.jpg" alt="" class="rounded-xl">
                         <h1 class="mt-2 text-xl font-medium">BMW M8</h1>
                         <h2>Brand: BMW</h2>
@@ -128,7 +260,7 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
     </div>
 </template>
 
@@ -144,10 +276,20 @@ export default {
         title(value) {
             this.header = value
         },
+        calulateHeight() {
+            let height = window.innerHeight;
+            let menuHeight = height - 300
+            let contentHeight = height - 402
+            document.getElementById("menu").style.height = menuHeight + "px";
+            document.getElementById("content").style.height = contentHeight + "px";
+        },
         price(value) {
             return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         }
     },
+    mounted() {
+        this.calulateHeight();
+    }
 }
 </script>
 
--- pages/setting/index.vue
+++ pages/setting/index.vue
@@ -93,7 +93,6 @@ export default {
     data() {
         return {
             header: "Account",
-            ize: ""
         }
     },
     mounted() {
